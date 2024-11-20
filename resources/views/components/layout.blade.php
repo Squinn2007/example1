379c8b678cceea13830fe955a736746700462578
Squinn2007 <95441681+Squinn2007@users.noreply.github.com>
2024-11-20 12:28:18 +0100

    new1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-      <select></select>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
